ad021fbde06f736872071684af3096d253cfa72e
user020603 <ptit.tienthanh@gmail.com>
2025-06-02 08:30:16 +0700

    feat(adm/cfg): add server config

---
+++ config/config.go
@@ -0,0 +1,65 @@
+package config
+
+import (
+	"os"
+	"sync"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	ServerPort    string
+	GrpcPort      string
+	DBHost        string
+	DBPort        string
+	DBUser        string
+	DBPassword    string
+	DBName        string
+	RedisAddr     string
+	RedisPassword string
+	EsAddr        string
+	KafkaBrokers  []string
+	KafkaTopic    string
+	KafkaGroupID  string
+	LogLevel      string
+	LogFile       string
+}
+
+var (
+	configInstance *Config
+	once           sync.Once
+)
+
+func LoadConfig() *Config {
+	once.Do(func() {
+		_ = godotenv.Load()
+
+		configInstance = &Config{
+			ServerPort:    getEnv("SERVER_PORT", "8001"),
+			GrpcPort:      getEnv("GRPC_PORT", "50051"),
+			DBHost:        getEnv("DB_HOST", "localhost"),
+			DBPort:        getEnv("DB_PORT", "5432"),
+			DBUser:        getEnv("DB_USER", "postgres"),
+			DBPassword:    getEnv("DB_PASSWORD", "password"),
+			DBName:        getEnv("DB_NAME", "admdb"),
+			RedisAddr:     getEnv("REDIS_ADDR", "localhost:6379"),
+			RedisPassword: getEnv("REDIS_PASSWORD", ""),
+			EsAddr:        getEnv("ES_ADDR", "http://localhost:9200"),
+			KafkaBrokers:  []string{getEnv("KAFKA_BROKERS", "localhost:9092")},
+			KafkaTopic:    getEnv("KAFKA_TOPIC", "server_topic"),
+			KafkaGroupID:  getEnv("KAFKA_GROUP_ID", "server_group"),
+			LogLevel:      getEnv("LOG_LEVEL", "info"),
+			LogFile:       getEnv("LOG_FILE", "logs/server.log"),
+		}
+	})
+
+	return configInstance
+}
+
+func getEnv(key, fallback string) string {
+	val := os.Getenv(key)
+	if val == "" {
+		return fallback
+	}
+	return val
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module thanhnt208/container-adm-service
 go 1.24.1
 
 require (
+	github.com/joho/godotenv v1.5.1
 	go.uber.org/zap v1.27.0
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1
 )
