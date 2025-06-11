cb6e89d4ac0ee5e7f3d610858629d6fc03fee784
user020603 <ptit.tienthanh@gmail.com>
2025-06-11 09:15:20 +0700

    update(adm/config): update jwt config

--- api/routes/container_routes.go
+++ api/routes/container_routes.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"thanhnt208/container-adm-service/api/middlewares"
 	"thanhnt208/container-adm-service/internal/delivery/rest"
 
 	"github.com/gin-gonic/gin"
@@ -11,8 +12,8 @@ func SetupContainerRoutes(h *rest.RestContainerHandler) *gin.Engine {
 
 	router.POST("/container/create", h.CreateContainer)
 	router.GET("/container/view", h.ViewContainers)
-	router.PUT("/container/update/:id", h.UpdateContainer)
-	router.DELETE("/container/delete/:id", h.DeleteContainer)
+	router.PUT("/container/update/:id", middlewares.JWTAuthMiddleware(), middlewares.AdminOnlyMiddleware(), h.UpdateContainer)
+	router.DELETE("/container/delete/:id", middlewares.JWTAuthMiddleware(), middlewares.AdminOnlyMiddleware(), h.DeleteContainer)
 	router.POST("/container/import", h.ImportContainers)
 	router.GET("/container/export", h.ExportContainers)
 
--- config/config.go
+++ config/config.go
@@ -2,27 +2,31 @@ package config
 
 import (
 	"os"
+	"strconv"
 	"sync"
 
 	"github.com/joho/godotenv"
 )
 
 type Config struct {
-	ServerPort    string
-	GrpcPort      string
-	DBHost        string
-	DBPort        string
-	DBUser        string
-	DBPassword    string
-	DBName        string
-	RedisAddr     string
-	RedisPassword string
-	EsAddr        string
-	KafkaBrokers  []string
-	KafkaTopic    string
-	KafkaGroupID  string
-	LogLevel      string
-	LogFile       string
+	ServerPort      string
+	GrpcPort        string
+	DBHost          string
+	DBPort          string
+	DBUser          string
+	DBPassword      string
+	DBName          string
+	RedisAddr       string
+	RedisPassword   string
+	EsAddr          string
+	KafkaBrokers    []string
+	KafkaTopic      string
+	KafkaGroupID    string
+	LogLevel        string
+	LogFile         string
+	JWTSecret       string
+	JWTExpiresIn    int
+	RefreshTokenTTL int
 }
 
 var (
@@ -34,22 +38,34 @@ func LoadConfig() *Config {
 	once.Do(func() {
 		_ = godotenv.Load()
 
+		jwtExpiresIn, err := strconv.Atoi(getEnv("JWT_EXPIRES_IN", "3600"))
+		if err != nil {
+			jwtExpiresIn = 3600
+		}
+		refreshTokenTTL, err := strconv.Atoi(getEnv("REFRESH_TOKEN_TTL", "604800"))
+		if err != nil {
+			refreshTokenTTL = 604800
+		}
+
 		configInstance = &Config{
-			ServerPort:    getEnv("SERVER_PORT", "8001"),
-			GrpcPort:      getEnv("GRPC_PORT", "50051"),
-			DBHost:        getEnv("DB_HOST", "localhost"),
-			DBPort:        getEnv("DB_PORT", "5432"),
-			DBUser:        getEnv("DB_USER", "postgres"),
-			DBPassword:    getEnv("DB_PASSWORD", "password"),
-			DBName:        getEnv("DB_NAME", "admdb"),
-			RedisAddr:     getEnv("REDIS_ADDR", "localhost:6379"),
-			RedisPassword: getEnv("REDIS_PASSWORD", ""),
-			EsAddr:        getEnv("ES_ADDR", "http://localhost:9200"),
-			KafkaBrokers:  []string{getEnv("KAFKA_BROKERS", "localhost:9092")},
-			KafkaTopic:    getEnv("KAFKA_TOPIC", "container_topic"),
-			KafkaGroupID:  getEnv("KAFKA_GROUP_ID", "container_group_id"),
-			LogLevel:      getEnv("LOG_LEVEL", "info"),
-			LogFile:       getEnv("LOG_FILE", "../../logs/container-adm.log"),
+			ServerPort:      getEnv("SERVER_PORT", "8001"),
+			GrpcPort:        getEnv("GRPC_PORT", "50051"),
+			DBHost:          getEnv("DB_HOST", "localhost"),
+			DBPort:          getEnv("DB_PORT", "5432"),
+			DBUser:          getEnv("DB_USER", "postgres"),
+			DBPassword:      getEnv("DB_PASSWORD", "password"),
+			DBName:          getEnv("DB_NAME", "admdb"),
+			RedisAddr:       getEnv("REDIS_ADDR", "localhost:6379"),
+			RedisPassword:   getEnv("REDIS_PASSWORD", ""),
+			EsAddr:          getEnv("ES_ADDR", "http://localhost:9200"),
+			KafkaBrokers:    []string{getEnv("KAFKA_BROKERS", "localhost:9092")},
+			KafkaTopic:      getEnv("KAFKA_TOPIC", "container_topic"),
+			KafkaGroupID:    getEnv("KAFKA_GROUP_ID", "container_group_id"),
+			LogLevel:        getEnv("LOG_LEVEL", "info"),
+			LogFile:         getEnv("LOG_FILE", "../../logs/container-adm.log"),
+			JWTSecret:       getEnv("JWT_SECRET", "supersecretkey"),
+			JWTExpiresIn:    jwtExpiresIn,
+			RefreshTokenTTL: refreshTokenTTL,
 		}
 	})
 
